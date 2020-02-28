64d8299ae0af2592c2115943b51029f4d5155e8c
Max Zamarripa <MZamarripa@dfsupply.com>
2020-02-28 09:59:55 -0800

    Add Logging to List Page

--- .NET/CoreFundamentals/CoreFundamentals/Pages/Restaurants/List.cshtml.cs
+++ .NET/CoreFundamentals/CoreFundamentals/Pages/Restaurants/List.cshtml.cs
@@ -4,6 +4,7 @@ using CoreFundamentals.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace CoreFundamentals.Pages.Restaurants
 {
@@ -11,20 +12,25 @@ namespace CoreFundamentals.Pages.Restaurants
     {
         private readonly IConfiguration _config;
         private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> logger;
+
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
         public ListModel(IConfiguration config, 
-                         IRestaurantData restaurantData)
+                         IRestaurantData restaurantData,
+                         ILogger<ListModel> logger)
         {
             _config = config;
             _restaurantData = restaurantData;
+            this.logger = logger;
         }
 
         public void OnGet()
         {
+            logger.LogError("Executing ListModel");
             Message = _config["Message"];
             Restaurants = _restaurantData.GetRestaurantsByName(SearchTerm);
         }
