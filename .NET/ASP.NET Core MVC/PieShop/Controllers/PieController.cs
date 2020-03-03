7ffa3d18799f7f6b4c8053db8fe21158202893f2
Max Zamarripa <MZamarripa@dfsupply.com>
2020-03-03 15:53:19 -0800

    Creating View, ViewModel, populate ViewModel in controller

--- .NET/ASP.NET Core MVC/PieShop/Controllers/PieController.cs
+++ .NET/ASP.NET Core MVC/PieShop/Controllers/PieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PieShop.Models;
+using PieShop.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,7 +23,11 @@ namespace PieShop.Controllers
 
         public ViewResult List()
         {
-            return View(_pieRepository.AllPies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+
+            return View(piesListViewModel);
         }
     }
 }
