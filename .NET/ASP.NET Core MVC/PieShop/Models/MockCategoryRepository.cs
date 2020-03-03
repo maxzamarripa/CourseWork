b3184bc920c9d077661a25f4c3d7ab2237559fcf
Max Zamarripa <MZamarripa@dfsupply.com>
2020-03-03 15:20:51 -0800

    Create Repositories Interfaces and Mocks

---
+++ .NET/ASP.NET Core MVC/PieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories => 
+            new List<Category> 
+            {
+                new Category
+                {
+                    CategoryId = 1,
+                    CategoryName = "Fruit pies",
+                    Description = "All-fruit pies"
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    CategoryName = "Cheese pies",
+                    Description = "Cheesy pies"
+                },
+                new Category
+                {
+                    CategoryId = 3,
+                    CategoryName = "Seasonal pies",
+                    Description = "Get in the season mood"
+                }
+            };
+    }
+}
