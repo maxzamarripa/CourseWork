ccd9d811b6784804da8f722b8d38190e4d0c0c1a
Max Zamarripa <MZamarripa@dfsupply.com>
2020-03-06 15:49:25 -0800

    Cart

--- .NET/ASP.NET Core MVC/PieShop/Startup.cs
+++ .NET/ASP.NET Core MVC/PieShop/Startup.cs
@@ -31,6 +31,10 @@ namespace PieShop
 
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -45,6 +49,8 @@ namespace PieShop
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
+
+            app.UseSession();
 
             app.UseRouting();
 
