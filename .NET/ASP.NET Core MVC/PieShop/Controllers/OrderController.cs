1a8d2d4fb16d17f06fe3eabfd4bc5ff28015768e
Max Zamarripa <MZamarripa@dfsupply.com>
2020-03-09 13:05:57 -0700

    Basic Form

---
+++ .NET/ASP.NET Core MVC/PieShop/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PieShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            this.orderRepository = orderRepository;
+            this.shoppingCart = shoppingCart;
+        }
+        // GET: /<controller>/
+        public IActionResult Checkout()
+        {
+
+            return View();
+        }
+    }
+}
