a0376ad81ce0f2459ecb0710b5d40bef50b4f081
Max Zamarripa <MZamarripa@dfsupply.com>
2020-03-09 12:51:58 -0700

    OrderRepostory + Move Repositories to it's own folder

---
+++ .NET/ASP.NET Core MVC/PieShop/Models/Repositories/OrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext appDbContext;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrderRepository(AppDbContext appDbContext, ShoppingCart shoppingCart)
+        {
+            this.appDbContext = appDbContext;
+            this.shoppingCart = shoppingCart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            appDbContext.Orders.Add(order);
+
+            var shoppingCartItems = shoppingCart.ShoppingCartItems;
+
+            foreach(var shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    Amount = shoppingCartItem.Amount,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    OrderId = order.OrderId,
+                    Price = shoppingCartItem.Pie.Price
+                };
+
+                appDbContext.OrderDetails.Add(orderDetail);
+            }
+
+            appDbContext.SaveChanges();
+        }
+    }
+}
