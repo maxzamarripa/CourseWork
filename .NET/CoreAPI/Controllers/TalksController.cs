df044f47725899d8477e083d69f494795e7a6989
Max Zamarripa <MZamarripa@dfsupply.com>
2020-03-17 13:31:08 -0700

    Talks API Controller + Get Talks by moniker

---
+++ .NET/CoreAPI/Controllers/TalksController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreAPI.Data;
+using CoreAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace CoreAPI.Controllers
+{
+    [Route("api/camps/{moniker}/[controller]")]
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository campRepository;
+        private readonly IMapper mapper;
+        private readonly LinkGenerator linkGenerator;
+
+        public TalksController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this.campRepository = campRepository;
+            this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string moniker)
+        {
+            try
+            {
+                var talks = await campRepository.GetTalksByMonikerAsync(moniker);
+
+                if (!talks.Any())
+                {
+                    return NotFound();
+                }
+
+                var response = mapper.Map<TalkModel[]>(talks);
+
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message + ex.InnerException?.Message);
+            }
+        }
+    }
+}
